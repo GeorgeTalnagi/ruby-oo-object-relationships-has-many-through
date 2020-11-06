536dc52a74262f4b60edb7c3e6876d4650f6459e
georgetalnagi <gtalnagi@yahoo.com>
2020-11-06 01:09:01 -0500

    commit tree cccd5bc178e9b4c660718ad39e3dd67d19b42440

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,36 @@
 class Customer
+   
+    attr_accessor :name, :age
   
-end
+    @@all = []
+  
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+  
+    def self.all
+        @@all
+    end
+  
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip)
+    end 
+
+    def meals
+        Meal.all.select do |meal|
+            meal.customer == self
+        end
+    end 
+
+    #Each meal is also associated with a waiter, so to get every waiter from 
+    #every meal Rachel has had, we need to take the array of all of Rachel's meals,
+    #map over it, getting the waiter from each of those meals.
+    def waiters
+        meals.map do |meal|
+          meal.waiter
+        end
+    end 
+
+end 
